98a60b8cf07a8ca79ffed2a8b67a0b814c59b7bd
KatarinaSundqvist <singsongkat@live.com>
2020-02-06 12:39:45 +0100

    Added validation

--- BethanysPieShop2/Models/Order.cs
+++ BethanysPieShop2/Models/Order.cs
@@ -1,21 +1,71 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BethanysPieShop2.Models {
     public class Order {
+        [BindNever]
         public int OrderId { get; set; }
+
         public List<OrderDetail> OrderDetails { get; set; }
+
+        [Required(ErrorMessage = "Please enter your first name")]
+        [Display(Name = "First Name")]
+        [StringLength(50)]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name")]
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your address")]
+        [Display(Name = "Address Line 1")]
+        [StringLength(100)]
         public string AddressLine1 { get; set; }
+
+        [Display(Name = "Address Line 2")]
+        [StringLength(100)]
         public string AddressLine2 { get; set; }
+
+        [Required(ErrorMessage = "Please enter your zip code")]
+        [Display(Name = "Zip Code")]
+        [StringLength(10, MinimumLength =4)]
         public string ZipCode { get; set; }
+        
+        [Required(ErrorMessage = "Please enter your city")]
+        [StringLength(50)]
         public string City { get; set; }
+        
+        [StringLength(20)]
         public string State { get; set; }
+        
+        [Required(ErrorMessage = "Please enter your country")]
+        [StringLength(50)]
         public string Country { get; set; }
+        
+        [Required(ErrorMessage = "Please enter your phone number")]
+        [Display(Name = "Phone Number")]
+        [StringLength(25)]
+        [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
+        
+        [Required(ErrorMessage = "Please enter your email address")]
+        [StringLength(100)]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage ="Please enter a valid email address")]
+        [RegularExpression(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])", ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
+
+        [BindNever]
+        [ScaffoldColumn(false)]
+        [Display(Name = "Order Total")]
         public decimal OrderTotal { get; set; }
+        
+        [BindNever]
+        [ScaffoldColumn(false)]
+        [Display(Name = "Order Placed")]
         public DateTime OrderPlaced { get; set; }
     }
 }
